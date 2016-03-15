a0e087e5e4e29538f3c7c2e587d480cf6c6aacdf
Jordi Fernandez <jordi.fernandez@y-nov.com>
2016-03-15 15:07:46 +0100

    Appels aux API OVH

---
+++ scripts/credentials.js
@@ -0,0 +1,31 @@
+// var config = require('../config/api-parameters.json');
+
+// for (var credentials in config) {
+
+// console.log(credentials['endpoint']);
+// console.log(credentials.endpoint);
+
+// var ovh = require('ovh')({
+//     endpoint: credentials.endpoint,
+//     appKey: credentials.application,
+//     appSecret: credentials.secret
+// });
+//
+// ovh.request('POST', '/auth/credential', {
+//     'accessRules': [{
+//         'method': 'GET',
+//         'path': '/*'
+//     }, {
+//         'method': 'POST',
+//         'path': '/*'
+//     }, {
+//         'method': 'PUT',
+//         'path': '/*'
+//     }, {
+//         'method': 'DELETE',
+//         'path': '/*'
+//     }]
+// }, function(error, credential) {
+//     console.log(error || credential);
+// });
+// }
