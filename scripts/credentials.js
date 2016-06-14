dd99b7a269855fcc4fbe7ee33cbc36b76cbbd666
Jordi Fernandez <yumiyum_@hotmail.fr>
2016-06-14 09:30:35 +0200

    Update credentials.js

--- scripts/credentials.js
+++ scripts/credentials.js
@@ -1,31 +1,29 @@
-// var config = require('../config/api-parameters.json');
+var config = require('../config/api-parameters.json');
 
-// for (var credentials in config) {
+for (var credentials in config) {
 
-// console.log(credentials['endpoint']);
-// console.log(credentials.endpoint);
+console.log(credentials['endpoint']);
 
-// var ovh = require('ovh')({
-//     endpoint: credentials.endpoint,
-//     appKey: credentials.application,
-//     appSecret: credentials.secret
-// });
-//
-// ovh.request('POST', '/auth/credential', {
-//     'accessRules': [{
-//         'method': 'GET',
-//         'path': '/*'
-//     }, {
-//         'method': 'POST',
-//         'path': '/*'
-//     }, {
-//         'method': 'PUT',
-//         'path': '/*'
-//     }, {
-//         'method': 'DELETE',
-//         'path': '/*'
-//     }]
-// }, function(error, credential) {
-//     console.log(error || credential);
-// });
-// }
+var ovh = require('ovh')({
+    endpoint: credentials.endpoint,
+    appKey: credentials.application,
+    appSecret: credentials.secret
+});
+
+ovh.request('POST', '/auth/credential', {
+    'accessRules': [{
+        'method': 'GET',
+        'path': '/*'
+      }, {
+         'method': 'POST',
+         'path': '/*'
+      }, {         'method': 'PUT',
+         'path': '/*'
+      }, {
+       'method': 'DELETE',
+        'path': '/*'
+      }]
+  }, function(error, credential) {
+      console.log(error || credential);
+  });
+}
